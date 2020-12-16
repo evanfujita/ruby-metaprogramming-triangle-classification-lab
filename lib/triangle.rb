b14f3b665800a586ddcded8c6c7f94ccf6dd57d3
Evan <evanfujita@gmail.com>
2020-12-16 12:55:07 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  attr_accessor :triangle
+  attr_reader :sides
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+    sides = @sides.sort
+    if sides[0] + sides[1] <= sides[2] ||
+      sides.any? {|side| side <= 0}
+        raise TriangleError
+    end
+  end
+
+  def kind
+    triangle = @sides.uniq.count
+    if triangle == 1
+      @triangle = :equilateral
+    elsif
+      triangle == 2
+      @triangle = :isosceles
+    elsif
+      triangle == 3
+      @triangle = :scalene
+    end
+    @triangle
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a triangle!"
+    end
+  end
 end
+ 
+a = Triangle.new(5, 5, 5)
+b = Triangle.new(5, 7, 5)
+c = Triangle.new(3, 4, 5)
+#d = Triangle.new(4, 3, 19)
